92a0ba2ee1f7c83c62705d4627eed39ce06e9271
khansohail425 <sohalkhan425@gmail.com>
2024-12-14 20:35:04 +0530

    Added unit test cases for MovieController

--- movies-info-service/src/test/java/com/reactivespring/controller/MovieInfoControllerUnitTest.java
+++ movies-info-service/src/test/java/com/reactivespring/controller/MovieInfoControllerUnitTest.java
@@ -17,6 +17,7 @@ import com.reactivespring.domain.MovieInfo;
 import com.reactivespring.services.MoviesInfoService;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @WebFluxTest(controllers = MovieInfoController.class)
 @AutoConfigureWebTestClient
@@ -34,6 +35,8 @@ class MovieInfoControllerUnitTest {
     void testDeleteMovieInfo() {
         String movieId = "abc";
         String url = String.format("%s/%s", MOVIES_INFO_BASE_URL, movieId);
+        when(moviesInfoService.deleteMovieInfo(eq(movieId))).thenReturn(Mono.empty());
+        when(moviesInfoService.getMovieById(eq(movieId))).thenReturn(Mono.empty());
         webClient.delete().uri(url).exchange().expectStatus().isNoContent();
         webClient.get().uri(url).exchange().expectStatus().is2xxSuccessful().expectBody().isEmpty();
     }
@@ -56,10 +59,15 @@ class MovieInfoControllerUnitTest {
                     assertNotNull(responseList);
                     assertEquals(3, responseList.size());
                 });
+
     }
 
     @Test
     void testGetMovieById() {
+
+        when(moviesInfoService.getMovieById(eq("abc"))).thenReturn(Mono.just(new MovieInfo("abc", "Dark Knight Rises",
+                2012, List.of("Christian Bale", "Tom Hardy"), LocalDate.parse("2012-07-20"))));
+
         String movieId = "/abc";
         String url = String.format("%s%s", MOVIES_INFO_BASE_URL, movieId);
         webClient.get().uri(url).exchange().expectStatus().is2xxSuccessful().expectBodyList(MovieInfo.class)
@@ -73,8 +81,10 @@ class MovieInfoControllerUnitTest {
 
     @Test
     void testSaveMovieInfo() {
-        MovieInfo requestBody = new MovieInfo(null, "Dark Knight Rises returns",
+        MovieInfo requestBody = new MovieInfo("demoId", "Dark Knight Rises returns",
                 2012, List.of("Christian Bale", "Tom Hardy"), LocalDate.parse("2012-07-20"));
+
+        when(moviesInfoService.saveMovieInfo(eq(requestBody))).thenReturn(Mono.just(requestBody));
 
         webClient.post().uri(MOVIES_INFO_BASE_URL).bodyValue(requestBody).exchange().expectStatus().isCreated()
                 .expectBody()
@@ -88,6 +98,8 @@ class MovieInfoControllerUnitTest {
         String url = String.format("%s/%s", MOVIES_INFO_BASE_URL, movieId);
         MovieInfo requestBody = new MovieInfo("abc", "Dark Knight Rises returns updated",
                 2012, List.of("Christian Bale", "Tom Hardy"), LocalDate.parse("2012-07-25"));
+
+        when(moviesInfoService.updateMovieInfo(isA(String.class), eq(requestBody))).thenReturn(Mono.just(requestBody));
 
         webClient.put().uri(url).bodyValue(requestBody).exchange().expectStatus().is2xxSuccessful().expectBody()
                 .jsonPath("$.name").isEqualTo("Dark Knight Rises returns updated").jsonPath("$.movieInfoId")
